3bdb1024d2ab01940847a929c65c27fa22f3a7f1
tramvay5401 <tramvay5401@gmail.com>
2020-07-05 18:56:02 +0300

    Add body-parser for auth

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const bodyParser  = require('body-parser')
 const app = express()
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 
 const authRoutes = require('./routes/auth')
 const lessonRoutes = require('./routes/lesson')
--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,9 @@
 module.exports.login = function (req, res) {
     res.status(200).json({
-        login: 'from controllers'
+        login: {
+            email:req.body.email,
+            password:req.body.password
+        }
     })
 }
 
