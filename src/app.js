b8ad7e86073d0203d66243426528405b18a9bf84
tramvay5401 <tramvay5401@gmail.com>
2020-07-05 19:02:57 +0300

    Add cors and morgan

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const bodyParser  = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
 const app = express()
 
+
+app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
+app.use(cors())
 
 const authRoutes = require('./routes/auth')
 const lessonRoutes = require('./routes/lesson')
