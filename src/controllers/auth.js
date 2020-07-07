92c0a8016a7fd24a8c058c4fb81df15f677dd027
tramvay5401 <tramvay5401@gmail.com>
2020-07-07 07:54:37 +0300

    Add registration functional

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,14 +1,38 @@
+const bcrypt = require('bcryptjs')
+
+
+const User = require('../models/User')
+
+
 module.exports.login = function (req, res) {
     res.status(200).json({
         login: {
-            email:req.body.email,
-            password:req.body.password
+            email: req.body.email,
+            password: req.body.password
         }
     })
 }
 
-module.exports.register = function (req, res) {
-    res.status(200).json({
-        register: 'from controllers'
-    })
+module.exports.register = async function (req, res) {
+    const candidate = await User.findOne({email: req.body.email})
+    if (candidate) {
+        res.status(409).json({
+            message: "This Email used, please try another "
+        })
+    } else {
+        const salt = bcrypt.genSaltSync(10)
+        const password = req.body.password
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password,salt),
+            name: req.body.name,
+            role: req.body.role
+        })
+        try {
+            await user.save()
+            res.status(201).json(user)
+        } catch (e) {
+            console.log(e)
+        }
+    }
 }
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,6 +3,7 @@ const controller = require('../controllers/auth')
 const router = express.Router()
 
 router.post('/login',controller.login)
+
 router.post('/register',controller.register)
 
 module.exports = router
