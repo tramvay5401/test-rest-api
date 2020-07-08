d3548da57b1d80fd86a274e10bc4b2274429f7a3
tramvay5401 <tramvay5401@gmail.com>
2020-07-08 09:22:58 +0300

    finished...

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const app = require('./app')
 
-
+require('dotenv').config()
 
 const PORT = process.env.PORT || 5000
+const HOST = process.env.HOST
 
-app.listen(PORT,()=>{
-    console.log(`Server has been started on ${PORT} port`)
+app.listen(PORT, HOST, ()=>{
+    console.log(`Server has been started on ${HOST} : ${PORT} `)
 })
